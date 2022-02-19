1d76e20259bc44223f77a8e8fcc606dbc6caf22f
andrebhu@gmail.com <andrebhu@gmail.com>
2022-02-19 04:14:23 -0500

    Barely working

--- src/app.py
+++ src/app.py
@@ -2,21 +2,15 @@
 
 from base64 import b64encode
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
+# from flask_bootstrap import Bootstrap
 import requests
+
+from retrieveImage import retrieveImage
 
 app = Flask(__name__)
 
-app.config["BOOTSTRAP_SERVE_LOCAL"] = True
-bootstrap = Bootstrap(app)
-
-
-def retrieveImage(tokenId):
-    '''
-    Given a tokenId, return corresponding image
-    '''
-
-    return
+# app.config["BOOTSTRAP_SERVE_LOCAL"] = True
+# bootstrap = Bootstrap(app)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -24,31 +18,34 @@ def index():
     if request.method == "POST":
         
         # Retrieve NFT information from page, rename `file` later
-        tokenId = request.form["tokenId"]
+        token_id = int(request.form["tokenId"])
+        contract_address = request.form["contractAddress"]
 
         # Information to send back to page
-        output = "" # output can be a string
+        output = "tbd" # output can be a string
         image = "" # image should be returned as a b64 encoded string
 
-        if tokenId:
+        if token_id:
             try:
                 # TODO: Check max file size of NFT? Could it crash the server?
                 # TODO: Function that retrieves image from NFT token
 
                 # just returns input
-                output = f"{tokenId}"
+                # output = f"{token_id}"
 
-                retrieveImage(output)
+                image = retrieveImage(contract_address, token_id)
 
                 # b64 encode file to pass back to page
                 # image = b64encode(file).decode("utf-8")
 
             # If anything goes wrong
-            except:
+            except Exception as e:
+                print(e)
                 output = "Invalid NFT"
         else:
             return render_template("index.html")
 
+        print(image[:50])
         return render_template("index.html", output=output, image=image)
 
     elif request.method == "GET":
--- src/retrieveImage.py
+++ src/retrieveImage.py
@@ -1,16 +1,15 @@
+from base64 import b64encode
 import requests
 import json
 import web3
+import re
+
 
 
 def fetchABI(contract_address):
     # Exports contract ABI in JSON
 
     url = "https://api.etherscan.io/api"
-#    ?module=contract
-#    &action=getabi
-#    &address=0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413
-#    &apikey=YourApiKeyToken"
 
     params = {
         "module": "contract",
@@ -22,11 +21,10 @@ def fetchABI(contract_address):
     response = requests.get(url, data=params)
     response_json = response.json()
     abi_json = json.loads(response_json['result'])
-    # result = json.dumps({"abi":abi_json}, indent=4, sort_keys=True)
-    # print(result)
+    
     return abi_json
 
-def retrieveImage(contract_address, token_id):
+def getImageUrl(contract_address, token_id):
     INFURA_URL = 'https://mainnet.infura.io/v3/63c3c9dcf79441d0916d436ff98a9040'
     w3 = web3.Web3(web3.HTTPProvider(INFURA_URL))
     
@@ -34,15 +32,36 @@ def retrieveImage(contract_address, token_id):
     try:
         abi = fetchABI(contract_address)
         contract = w3.eth.contract(address=contract_address, abi=abi)
-        print(contract.functions.tokenURI(token_id).call())
-    except:
-        print("Bad token :(")
+        url = contract.functions.tokenURI(token_id).call()
+
+        if 'ipfs://' in url:
+            data = json.loads(requests.get('https://ipfs.io/ipfs/' + url.split("//")[1]).text)
+            # print(data.keys())
+            for key in data.keys():
+                if 'image' in key:
+                    return data[key]
+            
+        elif 'https' in url:
+            data = json.loads(requests.get(url).text)
+            for key in data.keys():
+                if 'image' in key:
+                    return data[key]
 
 
+    except Exception as e:
+        print(e)
+        # print("Bad token format, we only support 721 atm")
+
+def retrieveImage(contract_address, token_id):
+    '''
+    Given a contract_address and tokenId, return image encoded in base64
+    '''
+    image_url = getImageUrl(contract_address, token_id)
+    image_file = requests.get(image_url).content
+    return b64encode(image_file).decode("utf-8")
     
 
-
-
-retrieveImage('0x1b79c7832ed9358E024F9e46E9c8b6f56633691B', 51)
-retrieveImage('0x5c3Cc8D8f5C2186d07D0bd9E5b463Dca507b1708', 4599)
+# print(retrieveImage('0x1b79c7832ed9358E024F9e46E9c8b6f56633691B', 51))
+# retrieveImage('0x5c3Cc8D8f5C2186d07D0bd9E5b463Dca507b1708', 4599)
 # retrieveImage('0x5c1a0cc6dadf4d0fb31425461df35ba80fcbc110', 3778)
+
